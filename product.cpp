eccc905557a6728cf608d465b18ee33f863c4eea
itsme-prashant <83328437+itsme-prashant@users.noreply.github.com>
2021-05-20 16:15:39 +0530

    product.cpp
    this file print the product off two numbers

---
+++ product.cpp
@@ -0,0 +1,11 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+  int a,b,product;
+  cout<<"enter value of a and b: ";
+  cin>>a>>b;
+  product = a*b;
+  cout<<"the product of a and b is: "<<product;
+  getch();
+}
